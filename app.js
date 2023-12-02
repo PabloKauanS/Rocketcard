b584c8d52ab9a6dfba99a87d5e69b41a21750bed
pablo kauan <pablokauanoficial@gmail.com>
2023-12-01 21:16:32 -0300

    rework-code

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ const btn_close = document.querySelector("[data-btn-close]");
 eventLister(btn_close, closeModal);
 
 const btn_send = document.querySelector("[data-btn-submit]");
-eventLister(btn_send, data);
+eventLister(btn_send, sendName);
 
-async function data(i) {
+async function sendName(i) {
   const target = i.target;
-  target.style.display = "none";
-  document.querySelector("[data-modal]").appendChild(loading());
-
-  //Request API
-  const name_input = document.querySelector("#ml-input").value;
-  const data_user = await API(name_input);
-  if (!data_user) {
-    document.querySelector(".loading").remove();
-    target.style.display = "block";
-    return;
-  }
+  startAnima(target);
+  const input_name = document.querySelector("#ml-input").value;
+  const data_user = await API(input_name);
+  if (!data_user) return stopAnima(target);
   addInfo(data_user);
   document.querySelector("[data-avatar]").onload = closeModal;
+}
+
+function startAnima(t) {
+  t.style.display = "none";
+  document.querySelector("[data-modal]").appendChild(loading());
+}
+function stopAnima(t) {
+  document.querySelector(".loading").remove();
+  t.style.display = "block";
 }
